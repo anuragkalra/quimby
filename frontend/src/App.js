ba45977815605ec9fe7b20e4874bb00a5e33c046
anuragkalra <anuragkalra@outlook.com>
2020-12-14 19:15:52 -0500

    reorganize

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.scss';
+import Records from './Records';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Employee Participation App</h1>
+      </header>
+      <section>
+        <Records />
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ startup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#docker-compose up  #background process?
+
+#cd quimby/backend  
+#node index.js  #background process?
+
+
+#cd quimby/frontend
+#(if necessary npm install)
+#yarn start #background process?
+
