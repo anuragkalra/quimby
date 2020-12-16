72e7619a2b4567318bc5f93e2d25176d7cdc8714
anuragkalra <anuragkalra@outlook.com>
2020-12-15 23:58:09 -0500

    cleanup

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
 const {pool} = require('./config')
-
-//var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
-//var testAPIRouter = require('./routes/testAPI');
-//var booksRouter = require('./routes/books') //Add the link to the books router
-
 var app = express();
 
 // view engine setup
@@ -23,61 +17,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-//app.use('/', indexRouter);
-//app.use('/users', usersRouter);
-//app.use('/testAPI', testAPIRouter);
-//app.use('/books', booksRouter) //Attach the books route to the app
-
-//Need to migrate to own file
-const getBooks = (request, response) => {
-  pool.query('SELECT * FROM participants', (error, results) => {
-  //pool.query('SELECT * FROM books', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
-
-//Need to migrate to own file
-const addBook = (request, response) => {
-  const {author, title} = request.body
-
-  pool.query(
-    'INSERT INTO books (author, title) VALUES ($1, $2)',
-    [author, title],
-    (error) => {
-      if (error) {
-        throw error
-      }
-      response.status(201).json({status: 'success', message: 'Book added.'})
-    },
-  )
-}
-
-const deleteBook = (request, response) => {
-  const id = parseInt(request.query.id) //request.params is deprecated
-  
-  pool.query('DELETE FROM books WHERE id = $1', [id], (error, results) => {
-  //pool.query('DELETE FROM books WHERE id = 2', (error, results) => {  
-    if (error) {
-      throw error
-    }
-    response.status(200).send(`Book deleted with ID: ${id}`)
-    //response.status(200).send(`Book deleted with ID: 2`)
-  })
-}
-
-app
-  .route('/booksSpecial')
-  // GET endpoint
-  .get(getBooks)
-  // POST endpoint
-  .post(addBook)
-  // DELETE endpoint
-  .delete(deleteBook)
-
 
 //GET /participants
 const getParticipants = (request, response) => {
